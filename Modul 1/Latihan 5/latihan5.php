35e8ab7e63ba54c3ca1bf49046df137d8e0c0e86
Kezhiareyva <85090045+KezhiaReyvaCE@users.noreply.github.com>
2021-09-06 10:52:16 +0700

    Create latihan5.php

---
+++ Modul 1/Latihan 5/latihan5.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <tittle>Variabel antar Halaman</tittle>
+    </head>
+    <body> 
+        <form action ="variabel_hasil.php" method="post">
+            Nama : <input type="text" name="nama"><br>
+            Email : <input type = "text" name="email"><br>
+            <input type = "submit" name = "submit" value = "Tampilkan">
+        </form>
+    </body>
+</html>
