339aaf4c76581067f813521cf634293b9f869ced
Kezhiareyva <85090045+KezhiaReyvaCE@users.noreply.github.com>
2021-09-06 10:45:22 +0700

    Create latihan2.php

---
+++ Modul 1/Latihan 2/latihan2.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <tittle>PHP dalam tag HTML </tittle>
+    </head>
+    <body>
+        <h1>
+            <?php
+                echo "Selamat Datang di SMK Telkom Sandhy Putra Malang";
+            ?>
+        </h1>
+    </body>
+</html>
